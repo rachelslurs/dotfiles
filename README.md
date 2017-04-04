36f3fd4f249eb7b487e8b0ce62fb168a78209121
Rachel Cantor <rachelcantor@gmail.com>
2017-04-04 11:46:07 -0400

    updating

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # dotfiles
-
-## brew
-`brew install bash grep nginx openssl@1.1 bash-completion@2 mongodb nvm pcre git mysql openssl readline`
---
+++ brew.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# specific work needs
+brew install bash grep nginx openssl@1.1 bash-completion@2 mongodb nvm pcre git mysql openssl readline
+
+# Remove outdated versions from the cellar.
+brew cleanup
